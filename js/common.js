0c7a402ebdfb37259b2a486d29a6bb43c7b7e853
erball <erbolmongol@gmail.com>
2024-03-17 22:41:16 -0700

    completed issue #11, added footer, faviocn, changed theme, fixed minor spelling mistakes

--- js/common.js
+++ js/common.js
@@ -4,9 +4,9 @@
     const iconLink = document.querySelector("#icon");
 
     styleLink.href =
-      "https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.3.3/cerulean/bootstrap.min.css";
+      "https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.3.3/vapor/bootstrap.min.css";
     iconLink.href =
-      "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.3/font/bootstrap-icons.min.css";
+      "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css";
   };
 
   document.addEventListener("DOMContentLoaded", () => {
